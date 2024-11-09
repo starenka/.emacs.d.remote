fd6e299b6b31d2ebd6d91686a75068e94b172299
starenka <starenka0@gmail.com>
2024-11-09 22:54:41 +0100

    doh

--- init.el
+++ init.el
@@ -337,19 +337,22 @@
               ("t" . sta:deadgrep-file-type)))
 
 (use-package sqlite-mode-extras
-           sqlite-mode-map
-           ("b" . sqlite-mode-extras-backtab-dwim)
-           ("f" . sqlite-mode-extras-tab-dwim)
-           ("+" . sqlite-mode-extras-add-row)
-           ("D" . sqlite-mode-extras-delete-row-dwim)
-           ("C" . sqlite-mode-extras-compose-and-execute)
-           ("E" . sqlite-mode-extras-execute)
-           ("S" . sqlite-mode-extras-execute-and-display-select-query)
-           ("DEL" . sqlite-mode-extras-delete-row-dwim)
-           ("g" . sqlite-mode-extras-refresh)
-           ("<backtab>" . sqlite-mode-extras-backtab-dwim)
-           ("<tab>" . sqlite-mode-extras-tab-dwim)
-           ("RET" . sqlite-mode-extras-ret-dwim)))
+  :bind (:map
+         sqlite-mode-map
+         ("n" . next-line)
+         ("p" . previous-line)
+         ("b" . sqlite-mode-extras-backtab-dwim)
+         ("f" . sqlite-mode-extras-tab-dwim)
+         ("+" . sqlite-mode-extras-add-row)
+         ("D" . sqlite-mode-extras-delete-row-dwim)
+         ("C" . sqlite-mode-extras-compose-and-execute)
+         ("E" . sqlite-mode-extras-execute)
+         ("S" . sqlite-mode-extras-execute-and-display-select-query)
+         ("DEL" . sqlite-mode-extras-delete-row-dwim)
+         ("g" . sqlite-mode-extras-refresh)
+         ("<backtab>" . sqlite-mode-extras-backtab-dwim)
+         ("<tab>" . sqlite-mode-extras-tab-dwim)
+         ("RET" . sqlite-mode-extras-ret-dwim)))
 
 
 (defun sta:deadgrep-file-type ()
