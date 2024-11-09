3c1a5902f95c32efcd04ae55165adcbdbf1132db
starenka <starenka0@gmail.com>
2024-11-09 22:51:17 +0100

    +sqlite

--- init.el
+++ init.el
@@ -336,6 +336,22 @@
   :bind (:map deadgrep-mode-map
               ("t" . sta:deadgrep-file-type)))
 
+(use-package sqlite-mode-extras
+           sqlite-mode-map
+           ("b" . sqlite-mode-extras-backtab-dwim)
+           ("f" . sqlite-mode-extras-tab-dwim)
+           ("+" . sqlite-mode-extras-add-row)
+           ("D" . sqlite-mode-extras-delete-row-dwim)
+           ("C" . sqlite-mode-extras-compose-and-execute)
+           ("E" . sqlite-mode-extras-execute)
+           ("S" . sqlite-mode-extras-execute-and-display-select-query)
+           ("DEL" . sqlite-mode-extras-delete-row-dwim)
+           ("g" . sqlite-mode-extras-refresh)
+           ("<backtab>" . sqlite-mode-extras-backtab-dwim)
+           ("<tab>" . sqlite-mode-extras-tab-dwim)
+           ("RET" . sqlite-mode-extras-ret-dwim)))
+
+
 (defun sta:deadgrep-file-type ()
   "Prompt the user for a new file type, then restart the search in deadgrep"
   (interactive)
